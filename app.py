4e6c4b8c98ff9d061094adca06de9d3928019fad
Khairul Akmal <khairulakmal1ua@gmail.com>
2024-05-24 11:31:27 +0900

    commit

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("my-profile.html")
+    return render_template("home.html")
 
-#@app.route("/login")
-#def login():
- #   return render_template("login.html")
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/my-profile")
+def login():
+    return render_template("my-profile.html")
