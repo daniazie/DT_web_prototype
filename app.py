57709ab5359376846ed6096587d7ccc83e4b0722
dania zie <dania.moriazi01@gmail.com>
2024-05-27 19:32:38 +0900

    commit

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 def index():
     return render_template("login.html")
 
-#@app.route("/login")
-#def login():
- #   return render_template("login.html")
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
