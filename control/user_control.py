a155bdc63ca3893baa30cba13f79b341d7650523
dania zie <dania.moriazi01@gmail.com>
2024-05-31 09:22:41 +0900

    commit

---
+++ control/user_control.py
@@ -0,0 +1,78 @@
+import base64
+import models.user as user
+import models.database as db
+
+def check_password(user_info, input):
+    encoded = base64.b64encode(input.encode("UTF-8")).decode("UTF-8")
+    return user_info.password == encoded
+
+def encode_password(input):
+    return base64.b64encode(input.encode("UTF-8")).decode("UTF-8")
+
+def is_exist_id(user_id):
+    connection = db.DataBase()
+    result = connection.execute_select("SELECT * FROM User WHERE id='{0}'".format(user_id))
+    if result: return True
+    else : return False
+
+def pull_user_info_from_db(user_id):
+    connection = db.DataBase()
+    data = user.User()
+    result = connection.execute_select("SELECT * FROM User WHERE id='{0}'".format(user_id))
+    if result:
+        data = user.User()
+        data.id = result['id']
+        data.password = result['password']
+        data.name = result['name']
+        data.email = result['email']
+        data.country = result['country']
+        data.gender = result['gender']
+        data.language = result['language']
+        data.city = result['city']
+        return data
+    else: # failed to search id from database
+        return None
+    
+def push_user_info_to_db(user_info):
+    connection = db.DataBase()
+    if is_exist_id(user_info.id):
+        sql = """UPDATE User SET 
+                    password='{1}', 
+                    name='{2}', 
+                    email='{3}', 
+                    country='{4}', 
+                    gender='{5}', 
+                    language='{6}', 
+                    city='{7}' 
+                 WHERE id='{0}'""".format(
+            user_info.id,
+            user_info.password,
+            user_info.name,
+            user_info.email,
+            user_info.country,
+            user_info.gender,
+            user_info.language,
+            user_info.city
+        )
+    else:
+        sql = "INSERT INTO dt.User (id, password, name, email, country, gender, language, city) \
+        VALUES ('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}')".format(
+            user_info.id,
+            user_info.password,
+            user_info.name,
+            user_info.email,
+            user_info.country,
+            user_info.gender,
+            user_info.language,
+            user_info.city
+        )
+    result,_ = connection.execute_else(sql)
+    return result
+
+def is_empty(*args):
+    result = False
+    
+    for i in args:
+        result = result or (i == "") or (i == None)
+    
+    return result
---
+++ models/user.py
@@ -0,0 +1,59 @@
+from flask_login import UserMixin
+import models.database as db
+
+class User(UserMixin): 
+    @property
+    def id(self):
+        return self.__id
+    @id.setter
+    def id(self,id):
+        self.__id = id
+
+    @property
+    def email(self):
+        return self.__email
+    @email.setter    
+    def email(self,email):
+        self.__email = email
+
+    @property
+    def country(self):
+        return self.__country
+    @country.setter    
+    def country(self,country):
+        self.__country = country
+
+    @property
+    def password(self):
+        return self.__password
+    @password.setter
+    def password(self,password):
+        self.__password = password
+
+    @property
+    def name(self):
+        return self.__name
+    @name.setter    
+    def name(self,name):
+        self.__name = name
+
+    @property
+    def gender(self):
+        return self.__gender
+    @gender.setter    
+    def gender(self,gender):
+        self.__gender = gender
+
+    @property
+    def language(self):
+        return self.__language
+    @language.setter    
+    def language(self,language):
+        self.__language = language
+
+    @property
+    def city(self):
+        return self.__city
+    @city.setter    
+    def city(self,city):
+        self.__city = city
