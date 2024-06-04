24c3e8377d20f95ce6714724f0588b22256e8c03
dkeke77 <ano0717@gmail.com>
2024-06-04 19:11:59 +0900

    home.html for문 적용

--- view/home_view.py
+++ view/home_view.py
@@ -3,11 +3,10 @@ from flask_login import login_required, login_manager
 from control import post_control
 from models import post
 
-
 home_view = Blueprint("home_view", __name__)
 
 @home_view.route("/home")
 @login_required
 def home():
     posts = post_control.pull_post_from_db_rows(3)
-    return render_template("home.html",list=posts)
+    return render_template("home.html",post_list=posts)
