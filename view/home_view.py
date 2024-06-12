65bf48a3931257db93bfa889e546b30c716c3fb0
dkeke77 <ano0717@gmail.com>
2024-06-13 04:16:55 +0900

    pull from remote

--- view/home_view.py
+++ view/home_view.py
@@ -1,12 +1,17 @@
 from flask import Flask, url_for, redirect, render_template, Blueprint
-from flask_login import login_required, login_manager
+from flask_login import login_required, login_manager, current_user, AnonymousUserMixin
 from control import post_control
 from models import post
 
 home_view = Blueprint("home_view", __name__)
 
 @home_view.route("/home")
-@login_required
 def home():
     posts = post_control.pull_post_from_db_rows(3)
-    return render_template("home.html",post_list=posts)
+    try:
+        if current_user.id:
+            print("logged in")
+            return render_template("home-user.html",post_list=posts)
+    except AttributeError:
+        print("not logged in")
+        return render_template("home-guest.html",post_list=posts)
