92e229a407bd03feb53d5725b2d5e996606ba935
dkeke77 <ano0717@gmail.com>
2024-06-19 00:28:35 +0900

    미구현 기능들 주석 처리 (사진, 번역, 필터링)

--- view/home_view.py
+++ view/home_view.py
@@ -9,11 +9,11 @@ home_view = Blueprint("home_view", __name__)
 def home():
     labels = lang_control.load_lang_dict("home",lang_control.selected_lang)
     posts = post_control.pull_post_from_db_rows(3)
-    my_comm_list = community_control.pull_community_list_from_db(current_user.id)
-    sug_comm_list = community_control.get_suggested_community_list(current_user.id,2)
     
     try:
         if current_user.id:
+            my_comm_list = community_control.pull_community_list_from_db(current_user.id)
+            sug_comm_list = community_control.get_suggested_community_list(current_user.id,2)
             return render_template("home/home-user.html",post_list=posts,labels=labels,user=current_user,
                                    my_comm_list=my_comm_list[:2],sug_comm_list=sug_comm_list)
     except AttributeError as e:
